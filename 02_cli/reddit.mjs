b44a4c5226b92e3ec39b42ccf287e46135732132
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-08-16 13:23:29 -0400

    add some console.log statements

--- 02_cli/reddit.mjs
+++ 02_cli/reddit.mjs
@@ -7,14 +7,17 @@ import yargs from 'yargs';
 
 // parse env vars
 const { argv } = yargs(process.argv);
+// console.log(argv);
 
 // init fetch to reddit api
 const res = await fetch('https://www.reddit.com/.json');
 const data = await res.json();
+// console.log(data);
 const randomIndex = Math.floor(Math.random() * data.data.children.length);
 
 // get random post from reddit api response of all posts on front page
 const post = data.data.children[randomIndex];
+// console.log(post);
 
 // log if "--print" flag is passed
 if (argv.print) {
